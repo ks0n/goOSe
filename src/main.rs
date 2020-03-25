53a6fc3fdd2ceee9e6eda383cdd85b0fba2fe94c
cohenarthur <arthur.cohen@epita.fr>
2020-03-25 21:26:44 +0100

    vga: Add correct buffer writing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 mod panic_handler;
 
-static GREET: &[u8] = b"Talk to me, Goose";
+static GREET: &[u8] = b"Talk to me, Goose !";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer_addr = 0xb8000 as *mut u8;
+unsafe {
+    let vga_buffer_addr = 0xb8000;
+    let mut i = 0;
 
-    unsafe {
-        for character in GREET {
-            core::ptr::write_volatile(vga_buffer_addr, *character as u8);
-        }
+    for character in GREET {
+        core::ptr::write_volatile((vga_buffer_addr + i) as *mut u8, *character as u8);
+        core::ptr::write_volatile((vga_buffer_addr + i + 1) as *mut u8, 0xa);
+        i += 2;
     }
 
+    asm!("cli");
+
     loop {}
+}
 }
