9ad749013656dd38be7be99fe3dff9dc3668db52
cohenarthur <arthur.cohen@epita.fr>
2020-03-25 21:15:41 +0100

    vga: Add basic VGA writing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@
 
 mod panic_handler;
 
+static GREET: &[u8] = b"Talk to me, Goose";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer_addr = 0xb8000 as *mut u8;
+
+    unsafe {
+        for character in GREET {
+            core::ptr::write_volatile(vga_buffer_addr, *character as u8);
+        }
+    }
+
     loop {}
 }
