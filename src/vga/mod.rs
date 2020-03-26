38ad63aecad095978c48986dda791632e11371d4
cohenarthur <arthur.cohen@epita.fr>
2020-03-26 18:04:15 +0100

    vga: Remove existing library

--- src/vga/mod.rs
+++ src/vga/mod.rs
@@ -1,25 +1,5 @@
-pub mod cell;
-pub mod attribute;
-pub mod write;
-pub mod buffer;
+static VGA_BUFFER_ADDR: u32 = 0xb8000;
 
-use crate::vga::buffer::Buffer;
-
-/// Write a string to the VGA buffer
 pub fn write(data: &str) -> usize {
-    let vga_buffer = Buffer::from_str(data);
-
-    return vga_buffer.write();
-}
-
-mod tests {
-    // FIXME: Add custom test framework that does not depend on `test`
-    /*
-    use super::*;
-
-    #[cfg(test)]
-    fn print_size() {
-        assert_eq!(print("Hey"), 3);
-    }
-    */
+    return 0;
 }
--- src/vga/write.rs
+++
@@ -1,7 +0,0 @@
-static VGA_BUFFER_ADDR: usize = 0xb8000;
-
-pub fn write_data(data: u8, index: usize) {
-    unsafe {
-        core::ptr::write_volatile((VGA_BUFFER_ADDR + index) as *mut u8, data);
-    }
-}
