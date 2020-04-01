e00d8c7b5990c35f04714e9d4dc9eaefab8762d1
cohenarthur <arthur.cohen@epita.fr>
2020-04-02 01:50:14 +0200

    serial: Add basic initialization function

--- src/serial/assembly.rs
+++ src/serial/assembly.rs
@@ -1,5 +1,26 @@
 pub static COM1: u16 = 0x3f8;
 
+/* Avoid unused code, but might be useful later
+pub static COM2: u16 = 0x2f8;
+pub static COM3: u16 = 0x3e8;
+pub static COM4: u16 = 0x2e8;
+
+pub static THR_OFF: u16 = 0x0;
+pub static RBR_OFF: u16 = 0x0;
+*/
+pub static DLL_OFF: u16 = 0x0;
+pub static DLH_OFF: u16 = 0x1;
+/*
+pub static IER_OFF: u16 = 0x1;
+pub static IIR_OFF: u16 = 0x2;
+pub static FCR_OFF: u16 = 0x2;
+pub static LCR_OFF: u16 = 0x3;
+pub static MCR_OFF: u16 = 0x4;
+pub static LSR_OFF: u16 = 0x5;
+pub static MSR_OFF: u16 = 0x6;
+pub static SR_OFF: u16 = 0x7;
+*/
+
 pub fn outb(port: u16, byte: u8) {
     unsafe {
         asm!("outb %al, %dx" ::
--- src/serial/mod.rs
+++ src/serial/mod.rs
@@ -7,6 +7,16 @@ pub fn syscall_write(data: &[u8], count: usize) {
     }
 }
 
+pub fn init_serial(port: u16) {
+    /* We initialize the Baude Rate of the port to 38400 bps */
+    assembly::outb(port + assembly::DLL_OFF, 0x3);
+    assembly::outb(port + assembly::DLH_OFF, 0x0);
+}
+
+pub fn init_com1() {
+    init_serial(assembly::COM1);
+}
+
 pub fn write_str(data: &str) {
     for byte in data.bytes() {
         assembly::outb(assembly::COM1, byte);
