e3503e0b0d52fe8f4308145fc394d9045f0b1204
Esteban Blanc <estblcsk@gmail.com>
2020-10-11 16:48:52 +0200

    cargo: Remove rlibc

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,7 +4,6 @@ cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use x86_64::*;
-        extern crate rlibc;
     }
     else if #[cfg(target_arch = "riscv64")] {
         mod riscv64;
