a4ce2d6b8ac00d1046304a608cee6a938a4bc44e
Esteban Blanc <estblcsk@gmail.com>
2020-10-28 22:43:21 +0100

    link: Fix stack alignment

--- src/allocator/mod.rs
+++ src/allocator/mod.rs
@@ -7,17 +7,14 @@ const PAGE_NUMBER: usize = 50;
 static mut PageUsage: [UsageFlags; PAGE_NUMBER] = [UsageFlags::Free; PAGE_NUMBER];
 
 #[repr(u8)]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum UsageFlags {
     Used = 1,
     Free = 0,
 }
 
 pub fn page_alloc() -> Option<usize> {
-    let mut index = 0;
-
-    // FIXME: Iterator
-    while index < 50 {
+    for index in 0..PAGE_NUMBER {
         unsafe {
             match PageUsage[index] {
                 UsageFlags::Free => {
@@ -29,10 +26,7 @@ pub fn page_alloc() -> Option<usize> {
                 UsageFlags::Used => (),
             }
         }
-
-        index = index + 1;
     }
-
     None
 }
 
@@ -40,9 +34,19 @@ pub fn page_alloc() -> Option<usize> {
 mod test {
     use super::*;
     use crate::utest::uassert_eq;
+
     #[test_case]
-    fn page_alloc_test() {
+    fn page_alloc_one_test() {
         let test = page_alloc();
-        kassert_eq!(test.is_some(), true, "Page alloc test");
+        kassert_eq!(test.is_some(), true, "Page alloc one page test");
+    }
+
+    #[test_case]
+    fn page_alloc_all_test() {
+        while page_alloc().is_some() {
+            // At some point we will not have any free pages left
+        }
+
+        kassert_eq!(true, true, "Page alloc all pages test");
     }
 }
