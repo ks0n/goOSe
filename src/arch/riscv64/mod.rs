0aae1fb0bad20df6e2bdc9f5f3c44df377131f64
CohenArthur <arthur.cohen@epita.fr>
2020-11-11 18:52:02 +0100

    arch/riscv64: Add module doc

--- src/arch/riscv64/mod.rs
+++ src/arch/riscv64/mod.rs
@@ -1,8 +1,12 @@
+//! RISCV64 module. Implements abstractions necessary to compile and use goOSe on
+//! RISCV64
+
 use super::*;
 use crate::kmain;
 use crate::println;
 use cfg_if::cfg_if;
 
+/// UART0 address
 pub static UART0: usize = 0x10000000;
 
 cfg_if! {
@@ -12,13 +16,13 @@ cfg_if! {
     }
 }
 
-/// Entry point of the kernel. Setup the stack address, call ['init'] and call ['kmain'].
+/// Entry point of the kernel. Setup the stack address, call `init` and call `kmain`.
 #[no_mangle]
 unsafe extern "C" fn kstart() -> ! {
     #[cfg(target_arch = "riscv64")]
     asm!(
         "la sp, STACK_START
-      call {}", sym init
+        call {}", sym init
     );
 
     kmain();
@@ -46,8 +50,9 @@ fn clear_bss() {
     println!("BSS cleared ({:#X} -> {:#X})", _bss_start, _bss_end);
 }
 
-/// Some architecture (x86...) have a specific instruction to write on some specific address (IO
-/// ports). RISCV don't so this is just a stub for writing at a specified address.
+/// Some architecture (x86...) have a specific instruction to write on some specific
+/// address (IO ports). RISCV does not, so this is just a stub for writing at
+/// a specified address.
 pub fn outb(addr: usize, byte: u8) {
     let addr = addr as *mut u8;
     unsafe {
