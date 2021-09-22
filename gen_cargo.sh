bc294dc9bc082416c1b4b2e1db9daf7e2fa51f25
Esteban Blanc <esteban.blanc@epita.fr>
2021-09-22 23:10:55 +0200

    build: Add script to generate cargo conf
    Fix riscv64_qemuvirt.conf
    
    Co-authored-by: Martin Schmidt <martin.schmidt@lse.epita.fr>

---
+++ gen_cargo.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+BOARDS_DIR='boards'
+
+usage() {
+    echo "Available boards:"
+    for board in "$BOARDS_DIR"/*.conf
+    do
+        board="${board##"$BOARDS_DIR"/}" # Remove prefix
+        board="${board%%.conf}" # Remove suffix
+        echo -e "\t$board"
+    done
+
+    echo "Usage: $0 <board>"
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+board="$1"
+board_conf="$BOARDS_DIR/$board.conf"
+
+if [ ! -e "$board_conf" ]; then
+    echo "Cannot find board: '$board' (looked for configuration file: $board_conf)"
+    exit 1
+fi
+
+mkdir -p .cargo
+ln -srf "$board_conf" .cargo/config
