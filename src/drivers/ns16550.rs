6f0d89a2d631eda9c1f6d8ff4bc87032ca4854e3
Martin Schmidt <martin.schmidt@lse.epita.fr>
2021-09-24 23:48:06 +0200

    Refactor serial with drivers directory and proper device name

---
+++ src/drivers/ns16550.rs
@@ -0,0 +1,27 @@
+/// The datasheet used to write this is:
+/// http://caro.su/msx/ocm_de1/16550.pdf
+
+pub const QEMU_VIRT_BASE_ADDRESS: usize = 0x10000000;
+
+pub struct Ns16550 {
+    base_register_address: usize,
+}
+
+impl Ns16550 {
+    pub fn new(base_register_address: usize) -> Self {
+        Self { base_register_address }
+    }
+
+    pub fn write(&self, data: &str) {
+        for byte in data.bytes() {
+            self.write_byte(byte);
+        }
+    }
+
+    fn write_byte(&self, byte: u8) {
+        let addr = self.base_register_address as *mut u8;
+        unsafe {
+            *addr = byte;
+        }
+    }
+}
