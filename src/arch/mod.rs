91694374a936ec4238473133dd8627a9e651e618
Esteban Blanc <esteban.blanc@epita.fr>
2021-09-24 18:11:26 +0200

    Add compile error when cargo not configured

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,14 +1,15 @@
 #[cfg(target_arch = "riscv64")]
 mod riscv64;
 
-pub fn new_arch() -> impl Architecture {
-    #[cfg(target_arch = "riscv64")]
-    return riscv64::Riscv64::new();
+use cfg_if::cfg_if;
 
-    #[allow(unreachable_code)]
-    {
-        // Block is needed as `unreachable!` will expand to multiple lines
-        unreachable!("Architecture not supported! Did you run `gen_cargo.sh`?");
+pub fn new_arch() -> impl Architecture {
+    cfg_if! {
+        if #[cfg(target_arch = "riscv64")] {
+            return riscv64::Riscv64::new();
+        } else {
+            core::compile_error!("Architecture not supported! Did you run `gen_cargo.sh`?");
+        }
     }
 }
 
