0df00a056b2230dcd9e2af8bf40b229297428138
Esteban Blanc <esteban.blanc@epita.fr>
2021-09-24 18:15:37 +0200

    driver: Add ns16550
    Refactor serial with drivers directory and proper device name
    ns16550: use volatile write
    ns16650: rename function to make code more device specific

---
+++ src/drivers/ns16550.rs
@@ -0,0 +1,27 @@
+/// The datasheet used to write this is:
+/// http://caro.su/msx/ocm_de1/16550.pdf
+
+pub const QEMU_VIRT_BASE_ADDRESS: usize = 0x10000000;
+
+pub struct Ns16550 {
+    base_register_address: usize,
+}
+
+impl Ns16550 {
+    pub fn new(base_register_address: usize) -> Self {
+        Self { base_register_address }
+    }
+
+    pub fn write(&self, data: &str) {
+        for byte in data.bytes() {
+            self.write_transmitter_holding_reg(byte);
+        }
+    }
+
+    fn write_transmitter_holding_reg(&self, byte: u8) {
+        // Transmitter Holding Register: +0 offset
+        let addr = (self.base_register_address + 0) as *mut u8;
+
+        unsafe { core::ptr::write_volatile(addr, byte) }
+    }
+}
