292a5d3ea708d81829199707feea30533c916078
Esteban Blanc <esteban.blanc@epita.fr>
2021-09-24 16:03:42 +0200

    Add riscv64 arch

---
+++ src/arch/mod.rs
@@ -0,0 +1,15 @@
+#[cfg(target_arch = "riscv64")]
+mod riscv64;
+
+pub fn new_arch() -> impl Architecture {
+    #[cfg(target_arch = "riscv64")]
+    return riscv64::Riscv64::new();
+
+    #[allow(unreachable_code)]
+    {
+        // Block is needed as `unreachable!` will expand to multiple lines
+        unreachable!("Architecture not supported! Did you run `gen_cargo.sh`");
+    }
+}
+
+pub trait Architecture {}
