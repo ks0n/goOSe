0bb61d14ed7d358c2ad45068b938548804c2ab8f
Esteban Blanc <esteban.blanc@epita.fr>
2021-09-26 22:50:06 +0200

    riscv: Add interrupts handler
    Add alignment to trap_handler
    
    Co-authored-by: Martin Schmidt <martin.schmidt@lse.epita.fr>

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,1 +1,2 @@
 pub mod ns16550;
+pub mod plic;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(asm)]
+#![feature(fn_align)]
 #![feature(naked_functions)]
 
 mod arch;
@@ -8,12 +9,24 @@ mod drivers;
 
 extern crate panic_halt;
 
+use arch::Architecture;
+
 #[no_mangle]
 fn k_main() -> ! {
-    let _arch = arch::new_arch();
+    let mut arch = arch::new_arch();
 
     let serial = drivers::ns16550::Ns16550::new(drivers::ns16550::QEMU_VIRT_BASE_ADDRESS);
     serial.write("GoOSe is booting\n\r");
+
+    arch.init_interrupts();
+
+    // unsafe {
+    //     enable_interrupt();
+    // }
+    // let mut plic = drivers::plic::Plic::new(drivers::plic::QEMU_VIRT_PLIC_BASE_ADDRESS);
+    // plic.set_threshold(0);
+    // plic.enable_interrupt(10);
+    // plic.set_priority(10, 1);
 
     loop {
         unsafe {
