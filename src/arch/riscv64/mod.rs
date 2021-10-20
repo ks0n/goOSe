0988c4f3237d49f13e9c3108895bf32e5e736fe4
Esteban Blanc <esteban.blanc@epita.fr>
2021-10-21 00:07:26 +0200

    riscv64: Add interrupt vector
    Co-authored-by: CohenArthur <cohenarthur.dev@gmail.com>

--- src/arch/riscv64/mod.rs
+++ src/arch/riscv64/mod.rs
@@ -1,7 +1,11 @@
 use super::Architecture;
+use crate::drivers::plic::plic_handler;
 
 #[no_mangle]
 static mut g_higher_trap_handler: *const () = 0 as *const ();
+
+static mut INTERRUPT_VECTOR: &[extern "C" fn()] =
+    &[wfi, wfi, wfi, wfi, wfi, wfi, wfi, wfi, wfi, plic_handler];
 
 pub struct Riscv64 {}
 
@@ -34,7 +38,7 @@ impl Riscv64 {
         }
     }
 
-    fn set_higher_trap_handler(&mut self, higher_trap_handler: fn()) {
+    fn set_higher_trap_handler(&mut self, higher_trap_handler: fn(cause: usize)) {
         unsafe {
             g_higher_trap_handler = higher_trap_handler as *const ();
         }
@@ -53,16 +57,33 @@ impl Architecture for Riscv64 {
         self.set_sie_ssie();
         self.set_sie_seie();
         self.set_stvec(trap_handler as usize);
+        self.set_higher_trap_handler(trampoline);
+    }
+}
 
-        self.set_higher_trap_handler(|| {
-            // well fuck we can't do anything without a context (like print a message on serial,
-            // but we can't since we don't have acces to the serial object.
-            // Should `init_interrupts` take a context parameters of some sort ?
-            // And don't you think taking in closures as trap handlers is pretty fucking sick ?!?
+fn is_interrupt(cause: usize) -> bool {
+    (cause >> 63) == 1
+}
+
+fn trampoline(cause: usize) {
+    match is_interrupt(cause) {
+        true => {
+            let exception_code = cause & !(1 << 63);
             unsafe {
-                asm!("wfi");
+                INTERRUPT_VECTOR[exception_code]();
             }
-        });
+        }
+        false => {
+            panic!("Exception not implemented yet");
+        }
+    }
+}
+
+extern "C" fn wfi() {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
     }
 }
 
@@ -72,9 +93,87 @@ impl Architecture for Riscv64 {
 unsafe extern "C" fn trap_handler() {
     asm!(
         "
-        ld t0, g_higher_trap_handler
+        addi sp, sp, -0x100
 
+        sd x31, 0x100(sp)
+        sd x30, 0xf8(sp)
+        sd x29, 0xf0(sp)
+        sd x28, 0xd8(sp)
+        sd x27, 0xd0(sp)
+        sd x26, 0xc8(sp)
+        sd x25, 0xc0(sp)
+        sd x24, 0xb8(sp)
+        sd x23, 0xb0(sp)
+        sd x22, 0xa8(sp)
+        sd x21, 0xa0(sp)
+        sd x20, 0x98(sp)
+        sd x19, 0x90(sp)
+        sd x18, 0x88(sp)
+        sd x17, 0x80(sp)
+        sd x16, 0x78(sp)
+        sd x15, 0x70(sp)
+        sd x14, 0x68(sp)
+        sd x13, 0x60(sp)
+        sd x12, 0x58(sp)
+        sd x11, 0x50(sp)
+        sd x10, 0x48(sp)
+        sd x9, 0x40(sp)
+        sd x8, 0x38(sp)
+        sd x7, 0x30(sp)
+        sd x6, 0x28(sp)
+        sd x5, 0x20(sp)
+        sd x4, 0x18(sp)
+        sd x3, 0x10(sp)
+        sd x2, 0x8(sp)
+        sd x1, 0x0(sp)
+
+        // mv a0, sp // Pointer on stack for the register struct
+        // csrr a1, sepc
+        // csrr a2, stval
+        // csrr a3, scause
+        // csrr a5, sstatus
+
+        csrr a0, scause
+        ld t0, g_higher_trap_handler
         jalr t0
+
+        csrr t0, sepc
+        addi t0, t0, 4
+        csrw sepc, t0
+
+        ld x1, 0x0(sp)
+        ld x2, 0x8(sp)
+        ld x3, 0x10(sp)
+        ld x4, 0x18(sp)
+        ld x5, 0x20(sp)
+        ld x6, 0x28(sp)
+        ld x7, 0x30(sp)
+        ld x8, 0x38(sp)
+        ld x9, 0x40(sp)
+        ld x10, 0x48(sp)
+        ld x11, 0x50(sp)
+        ld x12, 0x58(sp)
+        ld x13, 0x60(sp)
+        ld x14, 0x68(sp)
+        ld x15, 0x70(sp)
+        ld x16, 0x78(sp)
+        ld x17, 0x80(sp)
+        ld x18, 0x88(sp)
+        ld x19, 0x90(sp)
+        ld x20, 0x98(sp)
+        ld x21, 0xa0(sp)
+        ld x22, 0xa8(sp)
+        ld x23, 0xb0(sp)
+        ld x24, 0xb8(sp)
+        ld x25, 0xc0(sp)
+        ld x26, 0xc8(sp)
+        ld x27, 0xd0(sp)
+        ld x28, 0xd8(sp)
+        ld x29, 0xf0(sp)
+        ld x30, 0xf8(sp)
+        ld x31, 0x100(sp)
+
+        addi sp, sp, 0x100
 
         sret",
         options(noreturn)
--- src/drivers/plic.rs
+++ src/drivers/plic.rs
@@ -10,30 +10,29 @@ const PLIC_NUMBER_INTERRUPT_SOURCE_BY_REGISTER: u8 = 32;
 const PLIC_NUMBER_SOURCE_REGISTER: u16 =
     PLIC_NUMBER_SOURCES / PLIC_NUMBER_INTERRUPT_SOURCE_BY_REGISTER as u16;
 const PLIC_MAX_CONTEXT: u16 = 0x3e00;
+const PLIC_CLAIM_OFFSET: usize = 0x201004;
+
+static mut PLIC: Option<Plic> = None;
 
 pub struct Plic {
     base_register_address: usize,
-    threshold: u8,
 }
 
 impl Plic {
-    pub fn new(base_register_address: usize) -> Self {
+    pub const fn new(base_register_address: usize) -> Plic {
         Self {
             base_register_address,
-            threshold: 0,
         }
     }
 
-    pub fn set_threshold(&mut self, threshold: u8) {
-        self.threshold = threshold;
-
+    pub fn set_threshold(&self, threshold: u8) {
         unsafe {
             let addr = (self.base_register_address + PLIC_THRESHOLD_OFFSET) as *mut u32;
             addr.write_volatile(threshold as u32);
         }
     }
 
-    pub fn enable_interrupt(&self, id: u16, hart: u16) -> Result<(), &str> {
+    pub fn enable_interrupt(&self, id: u16, hart: u16) -> Result<(), &'static str> {
         if id >= PLIC_NUMBER_SOURCES {
             return Err("enable_interrupt: Id is higher than PLIC_MAX_INTERRUPT_SOURCE");
         }
@@ -56,7 +55,7 @@ impl Plic {
         Ok(())
     }
 
-    pub fn set_priority(&self, id: u16, priority: u32) -> Result<(), &str> {
+    pub fn set_priority(&self, id: u16, priority: u32) -> Result<(), &'static str> {
         if id >= PLIC_NUMBER_SOURCES {
             return Err("set_priority: Id is higher than PLIC_MAX_INTERRUPT_SOURCE");
         }
@@ -67,5 +66,44 @@ impl Plic {
         }
 
         Ok(())
+    }
+
+    pub fn claim(&self) -> u32 {
+        unsafe {
+            let addr = (self.base_register_address + PLIC_CLAIM_OFFSET) as *mut u32;
+            addr.read_volatile()
+        }
+    }
+
+    pub fn complete(&self, source: u32) {
+        unsafe {
+            let addr = (self.base_register_address + PLIC_CLAIM_OFFSET) as *mut u32;
+            addr.write_volatile(source);
+        }
+    }
+}
+
+pub fn init(base_register_address: usize) {
+    unsafe {
+        PLIC = Some(Plic::new(base_register_address));
+    }
+}
+
+pub fn get() -> &'static Plic {
+    let plic = unsafe { &PLIC };
+
+    match plic.as_ref() {
+        Some(plic_ref) => plic_ref,
+        None => unreachable!("PLIC should have been initialized at this point"),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn plic_handler() {
+    let plic = get();
+
+    let source = plic.claim();
+    if source == 10 {
+        plic.complete(source);
     }
 }
