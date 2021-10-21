495bc5cb2579ef5603c66d69105321e3e63797e9
Esteban Blanc <esteban.blanc@epita.fr>
2021-10-21 13:36:21 +0200

    plic: Add registering of interrupt handler
    Co-authored-by: CohenArthur <cohenarthur.dev@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,50 @@ use drivers::plic;
 
 use arch::Architecture;
 
+static mut KERNEL_SERIAL: Ns16550 = Ns16550::new(0);
+
+fn serial_write(data: &str) {
+    unsafe { KERNEL_SERIAL.write(data) };
+}
+fn serial_read() -> u8 {
+    unsafe { KERNEL_SERIAL.read() }
+}
+
+fn serial_interrupt_handler() {
+    let byte = serial_read();
+    let arr = [byte; 1];
+    serial_write(core::str::from_utf8(&arr).unwrap());
+}
+
 #[no_mangle]
 fn k_main() -> ! {
     let mut arch = arch::new_arch();
 
-    let serial = Ns16550::new(QEMU_VIRT_BASE_ADDRESS);
-    serial.write("GoOSe is booting\n\r");
+    unsafe {
+        KERNEL_SERIAL = Ns16550::new(QEMU_VIRT_BASE_ADDRESS);
+    }
+
+    serial_write("GoOSe is booting\n\r");
 
     arch.init_interrupts();
-
-    serial.enable_data_ready_interrupt();
 
     // Enable Serial interrupts
     plic::init(plic::QEMU_VIRT_PLIC_BASE_ADDRESS);
     let plic = plic::get();
     if let Err(e) = plic.set_priority(QEMU_VIRT_NS16550_INTERRUPT_NUMBER, 1) {
-        serial.write(e);
-        serial.write("\n\r");
+        serial_write(e);
+        serial_write("\n\r");
     }
     if let Err(e) = plic.enable_interrupt(QEMU_VIRT_NS16550_INTERRUPT_NUMBER, 0) {
-        serial.write(e);
-        serial.write("\n\r");
+        serial_write(e);
+        serial_write("\n\r");
     }
     plic.set_threshold(0);
+
+    unsafe {
+        KERNEL_SERIAL.enable_data_ready_interrupt();
+    }
+    plic.register_handler(10, serial_interrupt_handler);
 
     loop {
         unsafe {
