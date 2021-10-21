3769a1d4a6ebd6705a1357368b4cd2e884fdb287
Martin Schmidt <martin.schmidt@lse.epita.fr>
2021-10-21 14:33:09 +0200

    misc: Add conditional compilation for drivers

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,2 +1,13 @@
+#[cfg(drv_ns16550)]
 pub mod ns16550;
+
+#[cfg(drv_plic)]
 pub mod plic;
+
+// TODO: - have a driver trait
+//       - build a list of all the drivers we have compiled, list would countain `impl Driver`
+//         stuff, or a functions pointers to instantiate drivers.
+//         like a list of struct CompiledDriver {
+//                            name: &'static
+//                            driver: Fn(struct Driver &) -> impl Driver,
+//                        }
