2bf0bef3eb47ff0bc7cb5a070a490f16f91439c0
Esteban Blanc <esteban.blanc@epita.fr>
2022-06-23 22:04:59 +0200

    mm: Remove MemoryManager trait
    Co-authored-by: Martin Schmidt <martin.schmidt@lse.epita.fr>

--- src/mm/memory_management.rs
+++ src/mm/memory_management.rs
@@ -1,7 +1,7 @@
 use crate::arch;
 use crate::arch::ArchitectureMemory;
 use crate::mm;
-use crate::mm::{MemoryManager, PAddr, Permissions, VAddr};
+use crate::mm::{PAddr, Permissions, VAddr};
 
 pub struct MemoryManagement<'alloc> {
     arch: &'alloc mut arch::MemoryImpl,
@@ -14,33 +14,31 @@ impl<'alloc> MemoryManagement<'alloc> {
             arch: arch::MemoryImpl::new(&mut *page_allocator),
         }
     }
-}
 
-impl<'alloc> MemoryManager for MemoryManagement<'alloc> {
-    fn map(&mut self, phys: PAddr, virt: VAddr, perms: Permissions) {
+    pub fn map(&mut self, phys: PAddr, virt: VAddr, perms: Permissions) {
         let mut page_allocator = mm::get_global_allocator().lock();
 
         self.arch
             .map(&mut *page_allocator, phys.into(), virt.into(), perms)
     }
 
-    fn reload_page_table(&mut self) {
+    pub fn reload_page_table(&mut self) {
         self.arch.reload();
     }
 
-    fn disable_page_table(&mut self) {
+    pub fn disable_page_table(&mut self) {
         self.arch.disable();
     }
 
-    fn page_size(&self) -> usize {
+    pub fn page_size(&self) -> usize {
         arch::MemoryImpl::get_page_size()
     }
 
-    fn align_down(&self, addr: usize) -> usize {
+    pub fn align_down(&self, addr: usize) -> usize {
         arch::MemoryImpl::align_down(addr)
     }
 
-    fn align_up(&self, addr: usize) -> usize {
+    pub fn align_up(&self, addr: usize) -> usize {
         arch::MemoryImpl::align_up(addr)
     }
 }
