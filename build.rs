f1639625d0283f602890c805ba528e43eb268919
Martin Schmidt <martin.schmidt@lse.epita.fr>
2022-07-01 22:40:00 +0200

    refactor: Remove gen_cargo.sh in favor of a build.rs
    Also, since qemu virt platforms are similar, make the linkerscript
    common.

---
+++ build.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let linker_scripts = [
+        ("boards/generic_qemuvirt.ld",
+            ["riscv64_qemuvirt"]),
+    ];
+
+    for (linker_script, binaries) in linker_scripts.into_iter() {
+        for binary in binaries {
+            println!("cargo:rustc-link-arg-bin={}={}", binary, linker_script);
+            println!("cargo:rerun-if-changed={}", linker_script);
+        }
+    }
+}
