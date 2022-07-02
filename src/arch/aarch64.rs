f29d4dae741acda2d4f32fa04528b0ab542733ec
Martin Schmidt <martin.schmidt@lse.epita.fr>
2022-07-02 12:03:19 +0200

    refactor(device_tree): Extract all device_tree stuff into a module

--- src/arch/aarch64.rs
+++ src/arch/aarch64.rs
@@ -2,45 +2,12 @@ use core::arch::asm;
 
 use super::Architecture;
 
-pub struct Aarch64 {
-    device_tree: fdt::Fdt<'static>,
-}
+pub struct Aarch64 {}
 
 impl Architecture for Aarch64 {
     #[naked]
     #[no_mangle]
     unsafe extern "C" fn _start() -> ! {
         asm!("adrp x0, STACK_START", "mov sp, x0", "b k_main", options(noreturn));
-    }
-
-    fn new(info: usize) -> Self {
-        if let Ok(device_tree) = unsafe { fdt::Fdt::from_ptr(info as *const u8) } {
-            return Self { device_tree }
-        }
-
-        loop {}
-    }
-
-    fn for_all_memory_regions<F: FnMut(&mut dyn Iterator<Item = (usize, usize)>)>(&self, mut f: F) {
-        let memory = self.device_tree.memory();
-        let mut regions = memory
-            .regions()
-            .map(|region| (region.starting_address as usize, region.size.unwrap_or(0)));
-
-        f(&mut regions);
-    }
-
-    fn for_all_reserved_memory_regions<F: FnMut(&mut dyn Iterator<Item = (usize, usize)>)>(
-        &self,
-        mut f: F,
-    ) {
-        let reserved_memory = self.device_tree.find_node("/reserved-memory").unwrap();
-
-        let mut regions = reserved_memory
-            .children()
-            .flat_map(|child| child.reg().unwrap())
-            .map(|region| (region.starting_address as usize, region.size.unwrap_or(0)));
-
-        f(&mut regions);
     }
 }
