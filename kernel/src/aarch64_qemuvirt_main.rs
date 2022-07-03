ac9e2f183c0be29f51c0b9742f5d8e2358f7dfd6
Martin Schmidt <martin.schmidt@lse.epita.fr>
2022-07-03 17:40:28 +0200

    feat(aarch64): Add exception handling

--- kernel/src/aarch64_qemuvirt_main.rs
+++ kernel/src/aarch64_qemuvirt_main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(naked_functions)]
+#![feature(fn_align)]
 
 #[cfg(not(target_arch = "aarch64"))]
 compile_error!("Must be compiled as aarch64");
@@ -11,7 +12,9 @@ mod kernel_console;
 use arch::Architecture;
 use drivers::pl011::Pl011;
 
-use cortex_a::registers::*;
+use core::arch::asm;
+
+use cortex_a::{registers::*, asm};
 use tock_registers::interfaces::Writeable;
 
 pub type ConsoleImpl = Pl011;
@@ -24,7 +27,28 @@ extern "C" fn k_main(_device_tree_ptr: usize) -> ! {
 
     kernel_console::init(Pl011::new(0x0900_0000));
 
+    unsafe { arch::aarch64::Aarch64::init_el1_interrupts(); }
+
+    unsafe {
+        asm::barrier::isb(asm::barrier::SY);
+        asm::barrier::dmb(asm::barrier::SY);
+        asm::barrier::dsb(asm::barrier::SY);
+    };
+
     kprintln!("Kernel has been initialized");
+
+    if false {
+        // IRQ
+        DAIF.write(DAIF::D::Unmasked + DAIF::A::Unmasked + DAIF::I::Unmasked + DAIF::F::Unmasked);
+        CNTP_CTL_EL0.write(CNTP_CTL_EL0::ENABLE::SET + CNTP_CTL_EL0::IMASK::CLEAR + CNTP_CTL_EL0::ISTATUS::CLEAR);
+
+        unsafe { asm!("msr CNTP_CVAL_EL0, xzr") };
+        CNTP_TVAL_EL0.set(10000 as u64);
+
+    } else {
+        // Synchronous exception
+        unsafe { asm!("svc 42"); }
+    }
 
     loop {}
 }
