868c36c9ffe8733e80799e8f0fabefaa529df521
Esteban "Skallwar" Blanc <estblcsk@gmail.com>
2022-12-21 22:49:04 +0100

    misc: Add a flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "GoOSe devshell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-22.11";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              targets = ["aarch64-unknown-none" "riscv64gc-unknown-none-elf"];
+            }))
+            qemu
+          ];
+        };
+      }
+    );
+}
+
