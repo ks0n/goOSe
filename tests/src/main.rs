765675e6ef61226f37be17fc8fe78d076371955a
Esteban "Skallwar" Blanc <estblcsk@gmail.com>
2023-04-09 13:03:28 +0200

    tests: Add a test executable
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

---
+++ tests/src/main.rs
@@ -0,0 +1,20 @@
+#![no_main]
+#![no_std]
+#![feature(lang_items)]
+#![feature(fn_align)]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[no_mangle]
+#[repr(align(0x1000))]
+pub extern "C" fn _start() -> u8 {
+    0 as u8
+}
