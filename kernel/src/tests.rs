b9c783f90516dc59868c365a055bf9ec763303df
Martin Schmidt <martin.schmidt@lse.epita.fr>
2023-06-12 23:22:54 +0200

    organize in-kernel tests into into their own module

---
+++ kernel/src/tests.rs
@@ -0,0 +1,128 @@
+use log::{debug, info, trace};
+
+use core::sync::atomic::{AtomicUsize, Ordering};
+
+use super::drivers::{pl011::Pl011, Console};
+use crate::executable::elf::Elf;
+use crate::hal;
+use crate::mm::alloc_pages_for_hal;
+use hal_core::mm::{PageMap, Permissions};
+
+use align_data::include_aligned;
+use align_data::Align4K;
+
+pub enum TestResult {
+    Success,
+    Failure,
+}
+
+struct Test {
+    name: &'static str,
+    test: fn() -> TestResult,
+}
+
+const TESTS: &[Test] = &[
+    Test {
+        name: "timer interrupts",
+        test: test_timer_interrupt,
+    },
+    #[cfg(target_arch = "aarch64")]
+    Test {
+        name: "pagetable does remap",
+        test: test_pagetable_remap,
+    },
+    Test {
+        name: "basic elf loader",
+        test: test_elf_loader_basic,
+    },
+];
+
+pub fn launch() -> TestResult {
+    let mut res = TestResult::Success;
+
+    info!("Launching tests...");
+    for (i, test) in TESTS.iter().enumerate() {
+        info!("Test #{} \'{}\':", i, test.name);
+        match (test.test)() {
+            TestResult::Failure => {
+                info!("#{} failed ❌", i);
+                res = TestResult::Failure;
+            }
+            TestResult::Success => {
+                info!("#{} passed ✅", i);
+            }
+        }
+    }
+
+    res
+}
+
+fn test_timer_interrupt() -> TestResult {
+    if true {
+        // IRQ
+        static CNT: AtomicUsize = AtomicUsize::new(0);
+        const NUM_INTERRUPTS: usize = 3;
+
+        debug!(
+            "Testing timer interrupts, waiting for {} interrupts",
+            NUM_INTERRUPTS
+        );
+
+        hal::cpu::clear_physical_timer();
+
+        hal::irq::set_timer_handler(|| {
+            trace!(".");
+
+            if CNT.fetch_add(1, Ordering::Relaxed) < NUM_INTERRUPTS {
+                hal::irq::set_timer(50_000)
+                    .expect("failed to set timer in the timer handler of the test");
+            }
+        });
+
+        hal::irq::set_timer(50_000).expect("failed to set timer for test");
+
+        while CNT.load(Ordering::Relaxed) < NUM_INTERRUPTS {}
+
+        // TODO: restore the timer handler
+        hal::cpu::clear_physical_timer();
+        TestResult::Success
+    } else {
+        // // Synchronous exception
+        // unsafe {
+        //     asm!("svc 42");
+        // }
+        TestResult::Failure
+    }
+}
+
+fn test_pagetable_remap() -> TestResult {
+    info!("Testing the remapping capabilities of our pagetable...");
+    hal::mm::current()
+        .map(
+            hal_core::mm::VAddr::new(0x0450_0000),
+            hal_core::mm::PAddr::new(0x0900_0000),
+            Permissions::READ | Permissions::WRITE,
+            alloc_pages_for_hal,
+        )
+        .unwrap();
+    let uart = Pl011::new(0x0450_0000);
+    uart.write("Uart remaped, if you see this, it works !!!\n");
+
+    TestResult::Success
+}
+
+fn test_elf_loader_basic() -> TestResult {
+    static TEST_BIN: &[u8] = include_aligned!(Align4K, env!("CARGO_BIN_FILE_TESTS"));
+
+    let test_bin = Elf::from_bytes(TEST_BIN);
+    debug!("[OK] Elf from_bytes {}", env!("CARGO_BIN_FILE_TESTS"));
+    test_bin.load().unwrap();
+    debug!("[OK] Elf loaded");
+    let entry_point: extern "C" fn() -> u8 =
+        unsafe { core::mem::transmute(test_bin.get_entry_point()) };
+    debug!("[OK] Elf loaded, entry point is {:?}", entry_point);
+    entry_point();
+    debug!("[OK] Returned for Elf");
+
+    TestResult::Success
+}
