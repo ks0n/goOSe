29a6735e7156fb1afd0e4bd1649045ad810c87b1
Esteban "Skallwar" Blanc <estblcsk@gmail.com>
2024-10-08 13:48:22 +0200

    nix: update nixpkgs
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "GoOSe devshell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-24.05-small";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url  = "github:numtide/flake-utils";
   };
